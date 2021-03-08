dd44b9c9668673b7c9f5030af104cfc61bf95218
Christopher Simpson <Christopherleesimpson1@gmail.com>
2021-03-08 12:17:28 -0500

    Completed the section in chapter 2 called default values for URL parameters. Next is the Encode URL parameters.

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,8 +1,18 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
 
 $id = $_GET['id'] ?? '1'; // php 7.0 or later
 
 echo $id;
 
-
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
+
+
+
+
+
