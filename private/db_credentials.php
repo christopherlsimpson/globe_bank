4ad5cc1b6a30adc6de0e935cce6c5b5773953fb5
Christopher Simpson <Christopherleesimpson1@gmail.com>
2021-03-28 12:14:11 -0400

    Finished Chapter 7 with errors

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -5,5 +5,9 @@ define("DB_USER", "ChrisSimpson");
 define("DB_PASS", "Passw0rd!@#$");
 define("DB_NAME", "globe_bank");
 
+//define("DB_SERVER", "databasedesignguycom.startlogicmysql.com");
+//define("DB_USER", "globe_bank");
+//define("DB_PASS", "globe_bank");
+//define("DB_NAME", "globe_bank");
 
 ?>
--- private/query_functions.php
+++ private/query_functions.php
@@ -11,6 +11,83 @@ function find_all_subjects() {
 
 }
 
+function find_subject_by_id($id) {
+global $db;
+
+$sql = "SELECT * FROM subjects ";
+$sql .= "WHERE id='" . $id . "'";
+$result = mysqli_query($db, $sql);
+confirm_result_set($result);
+$subject = mysqli_fetch_assoc($result);
+mysqli_free_result($result);
+return $subject;
+
+}
+
+function insert_subject($subject) {
+global $db;
+
+$sql = "INSERT INTO subjects ";
+$sql .="(menu_name, position, visible) ";
+$sql .="VALUES (";
+$sql .="'" . $subject['menu_name'] . "',";
+$sql .="'" . $subject['position'] . "',";
+$sql .="'" . $subject['visible'] . "'";
+$sql .=")";
+$result = mysqli_query($db, $sql);
+
+if ($result) {
+return true;
+
+
+}else {
+echo mysqli_error($db);
+db_disconnect($db);
+exit;
+
+}
+}
+
+function update_subject($subject) {
+global $db;
+
+$sql = "UPDATE subjects SET ";
+  $sql .="menu_name='" . $subject['menu_name'] . "', ";
+  $sql .="position='" . $subject['position'] . "', ";
+  $sql .="visible='" . $subject['visible'] . "', ";
+  $sql .="WHERE id='" . $subject['id'] . "' ";
+  $sql .="LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+  if ($result) {
+  return true;
+
+  }else{
+  echo mysqli_error($db);
+  db_disconnect($db);
+  exit;
+  }
+
+}
+
+function delete_subject($id) {
+  Global $db;
+
+  $sql = "DELETE FROM subjects ";
+  $sql .="WHERE id='". $id ."' ";
+  $sql .="LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+
+  if ($result) {
+  return true;
+  }else{
+  echo mysqli_error($db);
+  db_disconnect($db);
+  exit;
+  }
+}
+
 function find_all_pages() {
     global $db;
 
@@ -21,7 +98,86 @@ function find_all_pages() {
     return $result;
   }
 
+function find_page_by_id($id) {
+  global $db;
 
+  $sql = "SELECT FROM pages ";
+  $sql .="WHERE id='" . $id . "'";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  $page = mysqli_fetch_assoc($result);
+  mysqli_free_result($result);
+  return $page;
+  
+}
+
+function insert_page($page) {
+  global $db;
+
+  $sql = "INSERT INTO pages ";
+  $sql .= "(subject_id, menu_name, position, visible, content) ";
+  $sql .= "VALUES (";
+  $sql .= "'" . $page['subject_id'] . "',";
+  $sql .= "'" . $page['menu_name'] . "',";
+  $sql .= "'" . $page['position'] . "',";
+  $sql .= "'" . $page['visible'] . "',";
+  $sql .= "'" . $page['content'] . "'";
+  $sql .= ")";
+
+  $result = mysqli_query($db, $sql);
+
+  if ($result) {
+    return true;
+  }else{
+  echo mysqli_error($db);
+  db_disconnect($db);
+  exit;
+  }
+
+}
+
+function update_page($page) {
+  global $db;
+
+  $sql = "UPDATE pages SET ";
+  $sql .= "subject_id='" . $page['subject_id'] . "', ";
+  $sql .= "menu_name='" . $page['menu_name'] . "', ";
+  $sql .= "position='" . $page['position'] . "', ";
+  $sql .= "visible='" . $page['visible'] . "', ";
+  $sql .= "content='" . $page['content'] . "' ";
+  $sql .= "WHERE id='" . $page['id'] . "' ";
+  $sql .= "LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+
+  if ($result) {
+    return true;
+  } else {
+  echo mysqli_error($db);
+  db_disconnect($db);
+  exit;  
+  }
+}
+
+
+function delete_page($id) {
+  global $db;
+
+  $sql = "DELETE FROM pages ";
+  $sql.= "WHERE id='" . $id . "' ";
+  $sql.= "LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+
+  if ($result) {
+    return true;
+  }else{
+    echo mysqli_error($db);
+    db_disconnect($db);
+    exit;   
+  }
+
+}
 
 
 ?>
