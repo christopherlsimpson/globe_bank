5f0cc64d68153187638f641e6971723578fdbda3
Christopher Simpson <Christopherleesimpson1@gmail.com>
2021-03-30 14:36:45 -0400

    Completed Chapter 9 No Errors

--- private/database.php
+++ private/database.php
@@ -15,6 +15,10 @@ function db_disconnect($connection) {
   }
 }
 
+function db_escape($connection, $string) {
+  return mysqli_real_escape_string($connection, $string);
+}
+
 function confirm_db_connect() {
     if(mysqli_connect_errno()) {
         $msg = "Database Connection Failed: ";
